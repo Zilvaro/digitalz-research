8921f639d8631b2ce64475c00e71261e9720705f
Zilvaro <zilvinas@digitalcase.ch>
2022-04-05 13:51:44 +0000

    Javascript functions high level descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,34 @@
 function setTwoNumberDecimal(event) {
     this.value = parseFloat(this.value).toFixed(2);
 }
+
+
+/**Name input check and task activation
+ * when name is entered and Start:
+ * - banner changes to Welcome 'name' & good luck
+ * - starts contdown clock
+ */
+
+/** Task activation function
+ * checks if name is entered
+ * if true - opens the task
+ * if not - alert: you have to enter the your name first
+ */
+
+/** Countdown clock
+ * - starts when name is entered
+ * - when the time is elapsed, the task is replaced with task graph
+ */
+
+/** guinness price graph
+ * - capture the entry and create 2 id: name:real, name:wished
+ * - create let(real) + let(wished random numbers) / id(random participants) 
+ * - display 4 bars with (xusers + name)real and (xusers + name) wished price
+ */
+
+/**vitamin research graph
+ * - capture the element clicked
+ * - create 5 random results (sum=100%) 
+ * - create the random number of #total answers  
+ * - create a graph showing the bottle pictures and the % of answers
+ */
