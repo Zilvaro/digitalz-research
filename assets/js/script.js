c91ac420cf8fd2650e5d427e740324dc37b1f2dc
Zilvaro <zilvinas@digitalcase.ch>
2022-04-06 08:32:37 +0000

    Timer function to specific date added

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,44 @@ function setTwoNumberDecimal(event) {
  * - when the time is elapsed, the task is replaced with task graph
  */
 
+ const deadline = 'April 07 2022 17:59:59 GMT+0200';
+
+ function getTimeRemaining(endtime){
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+  
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+
+  getTimeRemaining(deadline).minutes;
+
+  function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const timeinterval = setInterval(() => {
+      const t = getTimeRemaining(endtime);
+      clock.innerHTML =  t.days +' day(s) '+ t.hours + 'h : ' + t.minutes + 'm :' + t.seconds + 's';
+    
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    },1000);
+  }
+
+  initializeClock('clockdiv', deadline);
+
+
+
+
+
 /** guinness price graph
  * - capture the entry and create 2 id: name:real, name:wished
  * - create let(real) + let(wished random numbers) / id(random participants) 
