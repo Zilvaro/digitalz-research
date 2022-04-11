457d23187acb765ee1e86892c5a24d46c94a64c2
Zilvaro <zilvinas@digitalcase.ch>
2022-04-11 02:38:59 +0000

    Timer cookie store code

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,7 +57,24 @@ function setTwoNumberDecimal(event) {
 
   const timeInMinutes = 10;
   const currentTime = Date.parse(new Date());
-  const deadline = new Date(currentTime + timeInMinutes*60*1000);
+  let deadline;
+
+// if there's a cookie with the name myClock, use that value as the deadline
+if(document.cookie && document.cookie.match('myClock')){
+  // get deadline value from cookie
+  deadline = document.cookie.match(/(^|;)myClock=([^;]+)/)[2];
+} else {
+  // otherwise, set a deadline 10 minutes from now and 
+  // save it in a cookie with that name
+
+  // create deadline 10 minutes from now
+  const timeInMinutes = 10;
+  const currentTime = Date.parse(new Date());
+  deadline = new Date(currentTime + timeInMinutes*60*1000);
+
+  // store deadline in cookie for future reference
+  document.cookie = 'myClock=' + deadline + '; path=/; domain=https://zilvaro.github.io/digitalz-research/';
+}
 
   function getTimeRemaining(endtime){
     const total = Date.parse(endtime) - Date.parse(new Date());
